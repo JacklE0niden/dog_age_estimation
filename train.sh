ce78ac2c16c6b4a07db07392906f2b08916b565c
JackIE0niden <243981062@qq.com>
2024-12-02 14:47:47 +0800

    README和todo

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
-export PYTHONPATH=$PYTHONPATH:/mnt/pami26/zengyi/dlearning/dog_age_estimation
+# export PYTHONPATH=$PYTHONPATH:/mnt/pami26/zengyi/dlearning/dog_age_estimation
 # python -m torch.distributed.run --nproc_per_node=4 ./train/train.py
 # python ./train/train.py
 python -m torch.distributed.run --nproc_per_node=6 ./train/train_ddp.py
-torchrun --nproc_per_node=4 train_ddp.py
+# torchrun --nproc_per_node=4 train_ddp.py
 
-nohup python -m torch.distributed.run --nproc_per_node=6 ./train/train_ddp.py &> training_output.log &
+# nohup python -m torch.distributed.run --nproc_per_node=6 ./train/train_ddp.py &> training_output.log &
